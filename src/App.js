9d7d2697b50cd75982c13e63e410eb8d4e6aa0c1
Jonathan Robbins <Jonathan.Robbins@rcn.org.uk>
2021-10-22 12:02:41 +0100

    Added props and events

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import Backdrop from "./components/Backdrop";
+import Modal from "./components/Modal";
 import Todo from "./components/Todo";
 
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo />
-      <Todo />
+      <Todo text="Learn React" />
+      <Todo text="Learn Next" />
+      <Todo text="Learn Nuxt 3"/>
+      <Modal />
+      <Backdrop />
     </div>
   );
 }
