3e929a214d9887c8d19d274c09b58beefc4834f8
Jonathan Robbins <Jonathan.Robbins@rcn.org.uk>
2021-10-26 11:19:21 +0100

    Added links and main navigation

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import FavouritesPage from './pages/Favourites';
 import NewMeetupPage from './pages/NewMeetup';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
       <div>
+        <MainNavigation />
         <Switch>
           <Route path="/" exact={true}>
             <AllMeetupsPage />
